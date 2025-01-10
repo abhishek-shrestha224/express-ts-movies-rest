1bccdfca4df5ad807f53fa8b0f3ae17911b5011d
Abhishek Shrestha <meabhi224@gmail.com>
2025-01-10 10:31:33 +0545

    added createMovie to Controller

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Movie } from "../schemas/movieSchema.js";
+import { Movie, MovieCreate } from "../schemas/movieSchema.js";
 import movieService from "../services/movieService.js";
 
 const movieController = {
@@ -18,8 +18,24 @@ const movieController = {
                 Number(req.params.id)
             );
             res.status(200).send(movies);
-        } catch (err) {
+        } catch (_) {
             res.status(400).send("Something Went Wrong :(");
+        }
+    },
+    async createMovie(req: Request, res: Response) {
+        const { name, durationInMinutes, releasedYear } = req.body;
+        if (!name || !durationInMinutes || !releasedYear)
+            res.status(400).send("Bad Request! Missing some required data :(");
+        const movieData: MovieCreate = {
+            name: name,
+            durationInMinutes: durationInMinutes,
+            releasedYear: releasedYear,
+        };
+        try {
+            const newMovie = movieService.createNewMovie(movieData);
+            res.status(201).send({ msg: "Created :)", data: newMovie });
+        } catch (_) {
+            res.status(500).send("Something Went Wrong:(");
         }
     },
 };
