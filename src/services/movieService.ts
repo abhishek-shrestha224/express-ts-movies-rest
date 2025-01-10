cb51a14ba0244b288bbf99a503e8909acb9ab0a6
Abhishek Shrestha <meabhi224@gmail.com>
2025-01-10 10:26:06 +0545

    added createMovie method to Service

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,5 @@
 import fs from "fs/promises";
-import { Movie } from "../schemas/movieSchema.js";
+import { Movie, MovieCreate } from "../schemas/movieSchema.js";
 
 const movieService = {
     async getAllMovies(): Promise<Movie[]> {
@@ -30,6 +30,27 @@ const movieService = {
             return result;
         } catch (err) {
             console.error("Error reading movies data:", (err as Error).message);
+            throw new Error(
+                `Failed to fetch movies: ${(err as Error).message}`
+            );
+        }
+    },
+    async createNewMovie(createData: MovieCreate): Promise<Movie> {
+        try {
+            const movies: Movie[] = JSON.parse(
+                await fs.readFile("./src/db/movies.json", "utf8")
+            );
+            console.log(movies);
+
+            const newMovie: Movie = {
+                id: movies.length + 1,
+                ...createData,
+            };
+            movies.push(newMovie);
+
+            return newMovie;
+        } catch (err) {
+            console.error("Error writing movies data", (err as Error).message);
             throw new Error(
                 `Failed to fetch movies: ${(err as Error).message}`
             );
